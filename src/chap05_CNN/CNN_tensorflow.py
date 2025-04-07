3c3558010ada1066b614808ac8af2bd0b87c8810
wyl-kid <3182761324@qq.com>
2025-04-07 21:03:15 +0800

    Update CNN_tensorflow.py
    添加了提示

--- src/chap05_CNN/CNN_tensorflow.py
+++ src/chap05_CNN/CNN_tensorflow.py
@@ -3,14 +3,14 @@
 
 # In[ ]:
 
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 learning_rate = 1e-4
-keep_prob_rate = 0.7 
+keep_prob_rate = 0.7 # 
 max_epoch = 2000
-
 def compute_accuracy(v_xs, v_ys):
     global prediction
     y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
@@ -28,9 +28,15 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 def conv2d(x, W):
+    # 每一维度  滑动步长全部是 1， padding 方式 选择 same
+    # 提示 使用函数  tf.nn.conv2d
+    
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
+    # 滑动步长 是 2步; 池化窗口的尺度 高和宽度都是2; padding 方式 请选择 same
+    # 提示 使用函数  tf.nn.max_pool
+    
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 # define placeholder for inputs to network
@@ -39,19 +45,22 @@ ys = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
 x_image = tf.reshape(xs, [-1, 28, 28, 1])
 
-# 卷积层 1
+#  卷积层 1
+## conv1 layer ##
+
 W_conv1 = weight_variable([7, 7, 1, 32])
 b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
-
+ 
 # 卷积层 2
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-# 全连接层 1
+#  全连接层 1
+## fc1 layer ##
 W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 
@@ -60,9 +69,11 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # 全连接层 2
+## fc2 layer ##
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
 
 # 交叉熵函数
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
@@ -78,4 +89,5 @@ with tf.Session() as sess:
         sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob:keep_prob_rate})
         if i % 100 == 0:
             print(compute_accuracy(
-                mnist.test.images[:1000], mnist.test.labels[:1000])) 
+                mnist.test.images[:1000], mnist.test.labels[:1000]))
+
