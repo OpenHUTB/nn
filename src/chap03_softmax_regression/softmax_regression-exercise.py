b1d1917e74841fdc80222e7e95edc19c57eea713
fanxiangxiang <2680455468@qq.com>
2025-05-29 19:47:09 +0800

    Update softmax_regression-exercise.py
    在SoftmaxRegression类的__init__方法中，我初始化了self.w和self.b：
    填空二：在compute_loss函数中，我手动实现了softmax交叉熵损失

--- src/chap03_softmax_regression/softmax_regression-exercise.py
+++ src/chap03_softmax_regression/softmax_regression-exercise.py
@@ -43,41 +43,37 @@ plt.scatter(C3[:, 0], C3[:, 1], c='r', marker='*') # 绘制负样本，用红色
 data_set = np.concatenate((C1, C2, C3), axis=0) # 将正样本和负样本连接成一个数据集
 np.random.shuffle(data_set) # 随机打乱数据集的顺序
 
-
+epsilon = 1e-12
+class SoftmaxRegression():
+    def __init__(self):
 # ## 建立模型
 # 建立模型类，定义loss函数，定义一步梯度下降过程函数
 # 
 # 填空一：在`__init__`构造函数中建立模型所需的参数
-# 
-# 填空二：实现softmax的交叉熵损失函数(不使用tf内置的loss 函数)
-
-# In[1]:
-
-
-epsilon = 1e-12
-class SoftmaxRegression():
-    def __init__(self):
-        '''============================='''
-        #todo 填空一，构建模型所需的参数 self.W, self.b 可以参考logistic-regression-exercise
-        self.W = tf.Variable(shape=[2, 3], dtype=tf.float32, 
+#  self.W = tf.Variable(shape=[2, 3], dtype=tf.float32, 
                 initial_value=tf.random.uniform(shape=[2, 3], minval=-0.1, maxval=0.1))
         self.b = tf.Variable(shape=[3], dtype=tf.float32, initial_value=tf.zeros(shape=[3]))
         '''============================='''
         
         self.trainable_variables = [self.W, self.b]
+    
     @tf.function
     def __call__(self, inp):
         logits = tf.matmul(inp, self.W) + self.b # shape(N, 3)
         pred = tf.nn.softmax(logits)
         return pred    
-    
+ 
 @tf.function
 def compute_loss(pred, label):
     label = tf.one_hot(tf.cast(label, dtype=tf.int32), dtype=tf.float32, depth=3)
-    '''============================='''
-    #输入label shape(N, 3), pred shape(N, 3)
-    #输出 losses shape(N,) 每一个样本一个loss
-    #todo 填空二，实现softmax的交叉熵损失函数(不使用tf内置的loss 函数)
+
+
+
+
+
+# 填空二：实现softmax的交叉熵损失函数(不使用tf内置的loss 函数)
+# 修改说明：手动实现softmax交叉熵损失
+    # 添加epsilon防止log(0)的情况
     pred = tf.clip_by_value(pred, epsilon, 1.0)
     losses = -tf.reduce_sum(label * tf.math.log(pred), axis=1)
     '''============================='''
@@ -85,7 +81,7 @@ def compute_loss(pred, label):
     
     accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(label,axis=1), tf.argmax(pred, axis=1)), dtype=tf.float32))
     return loss, accuracy
-
+ 
 @tf.function
 def train_one_step(model, optimizer, x, y):
     with tf.GradientTape() as tape:
@@ -96,6 +92,17 @@ def train_one_step(model, optimizer, x, y):
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
     return loss, accuracy
 
+
+
+
+
+
+
+
+
+
+
+# In[1]:
 
 # ### 实例化一个模型，进行训练
 
