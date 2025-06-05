9da459204c8db0d6540c37ae02657139b083945b
tyqf <163406534+tyqf@users.noreply.github.com>
2025-06-05 23:04:52 +0800

    调整了导入语句的分组和间距
    在函数和类定义前后添加了空行
    调整了导入语句的分组和间距
    调整了长行的换行和缩进
    在运算符周围添加了空格

--- src/chap06_RNN/Learn2Carry-exercise.py
+++ src/chap06_RNN/Learn2Carry-exercise.py
@@ -13,7 +13,9 @@ import collections
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import layers, optimizers, datasets
-import os,sys,tqdm
+import os
+import sys
+import tqdm
 
 
 # ## 数据生成
@@ -25,6 +27,7 @@ import os,sys,tqdm
 #
 
 # In[2]:
+
 
 def gen_data_batch(batch_size, start, end):
     '''在(start, end)区间采样生成一个batch的整型的数据
@@ -38,6 +41,7 @@ def gen_data_batch(batch_size, start, end):
     results = numbers_1 + numbers_2
     return numbers_1, numbers_2, results
 
+
 def convertNum2Digits(Num):
     '''将一个整数转换成一个数字位的列表,例如 133412 ==> [1, 3, 3, 4, 1, 2]
     '''
@@ -45,6 +49,7 @@ def convertNum2Digits(Num):
     chNums = list(strNum)
     digitNums = [int(o) for o in strNum]
     return digitNums
+
 
 def convertDigits2Num(Digits):
     '''将数字位列表反向， 例如 [1, 3, 3, 4, 1, 2] ==> [2, 1, 4, 3, 3, 1]
@@ -54,11 +59,13 @@ def convertDigits2Num(Digits):
     Num = int(numStr)
     return Num
 
+
 def pad2len(lst, length, pad=0):
     '''将一个列表用`pad`填充到`length`的长度,例如 pad2len([1, 3, 2, 3], 6, pad=0) ==> [1, 3, 2, 3, 0, 0]
     '''
-    lst+=[pad]*(length - len(lst))
+    lst += [pad] * (length - len(lst))
     return lst
+
 
 def results_converter(res_lst):
     '''将预测好的数字位列表批量转换成为原始整数
@@ -67,6 +74,7 @@ def results_converter(res_lst):
     '''
     res = [reversed(digits) for digits in res_lst]
     return [convertDigits2Num(digits) for digits in res]
+
 
 def prepare_batch(Nums1, Nums2, results, maxlen):
     '''准备一个batch的数据，将数值转换成反转的数位列表并且填充到固定长度
@@ -95,18 +103,19 @@ def prepare_batch(Nums1, Nums2, results, maxlen):
     return Nums1, Nums2, results
 
 
-
 # # 建模过程， 按照图示完成建模
 
 # In[3]:
 
+
 class myRNNModel(keras.Model):
     def __init__(self):
         super(myRNNModel, self).__init__()
-        self.embed_layer = tf.keras.layers.Embedding(10, 32,
-                                                    batch_input_shape = [None, None])
+        self.embed_layer = tf.keras.layers.Embedding(
+            10, 32, batch_input_shape=[None, None])
         self.rnncell = tf.keras.layers.SimpleRNNCell(64)
-        self.rnn_layer = tf.keras.layers.RNN(self.rnncell, return_sequences = True)
+        self.rnn_layer = tf.keras.layers.RNN(
+            self.rnncell, return_sequences=True)
         self.dense = tf.keras.layers.Dense(10)
 
     @tf.function
@@ -119,11 +128,13 @@ class myRNNModel(keras.Model):
 
 # In[4]:
 
+
 @tf.function
 def compute_loss(logits, labels):
     losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits = logits , labels = labels)
+        logits=logits, labels=labels)
     return tf.reduce_mean(losses)
+
 
 @tf.function
 def train_one_step(model, optimizer, x, y, label):
@@ -136,31 +147,36 @@ def train_one_step(model, optimizer, x, y, label):
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
     return loss
 
+
 def train(steps, model, optimizer):
     loss = 0.0
     accuracy = 0.0
     for step in range(steps):
         datas = gen_data_batch(batch_size=200, start=0, end=555555555)
         Nums1, Nums2, results = prepare_batch(*datas, maxlen=11)
-        loss = train_one_step(model, optimizer, tf.constant(Nums1, dtype=tf.int32),
-                              tf.constant(Nums2, dtype=tf.int32),
-                              tf.constant(results, dtype=tf.int32))
-        if step%50 == 0:
+        loss = train_one_step(model, optimizer,
+                             tf.constant(Nums1, dtype=tf.int32),
+                             tf.constant(Nums2, dtype=tf.int32),
+                             tf.constant(results, dtype=tf.int32))
+        if step % 50 == 0:
             print('step', step, ': loss', loss.numpy())
-            
+
     return loss
+
 
 def evaluate(model):
     datas = gen_data_batch(batch_size=2000, start=555555555, end=999999999)
     Nums1, Nums2, results = prepare_batch(*datas, maxlen=11)
-    logits = model(tf.constant(Nums1, dtype=tf.int32), tf.constant(Nums2, dtype=tf.int32))
+    logits = model(tf.constant(Nums1, dtype=tf.int32),
+                   tf.constant(Nums2, dtype=tf.int32))
     logits = logits.numpy()
     pred = np.argmax(logits, axis=-1)
     res = results_converter(pred)
     for o in list(zip(datas[2], res))[:20]:
-        print(o[0], o[1], o[0]==o[1])
+        print(o[0], o[1], o[0] == o[1])
 
-    print('accuracy is: %g' % np.mean([o[0]==o[1] for o in zip(datas[2], res)]))
+    print('accuracy is: %g' % np.mean([o[0] == o[1]
+                                      for o in zip(datas[2], res)]))
 
 
 # In[5]:
