3c3136b49ca3f96d4e2772e093470cf6e2ece742
康达姆123 <2533726755@qq.com>
2025-06-06 20:39:55 +0800

    添加注释
    添加注释

--- src/chap14_reinforcement_learning/RL_QG_agent.py
+++ src/chap14_reinforcement_learning/RL_QG_agent.py
@@ -42,7 +42,7 @@ class RL_QG_agent: #定义了一个名为 RL_QG_agent 的类
 
     # 第2个卷积层：提取更高级特征
         conv2 = tf.layers.conv2d(
-            inputs = conv1,
+            inputs = conv1,             # 上一层（conv1）的输出作为当前卷积层的输入
             filters = 64,                 # 输出通道数：64个卷积核
             kernel_size = 3,             #指的是卷积核的大小为 3×3
             padding = "same",            #这种填充方式能保证输出特征图的尺寸和输入特征图的尺寸相同
