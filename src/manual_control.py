5257f2466fe95fcfc901804c65df47530f188f71
11lllll <160112674+11lllll@users.noreply.github.com>
2025-06-09 10:23:55 +0800

    Update manual_control.py

--- src/manual_control.py
+++ src/manual_control.py
@@ -220,7 +220,7 @@ class World(object): # Carla 仿真世界的核心管理类，负责初始化和
         self.radar_sensor = None   # 雷达传感器
         self.camera_manager = None # 相机管理器
         self._weather_presets = find_weather_presets()
-        self._weather_index = 0
+        self._weather_index = 0    #初始化当前天气预设
         self._actor_filter = args.filter
         self._actor_generation = args.generation
         self._gamma = args.gamma
