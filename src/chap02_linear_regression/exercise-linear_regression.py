00dbfe915114159347374e92b43c3525f58610fc
ASL505 <1467212151@qq.com>
2025-06-15 16:59:02 +0800

    add annotation3

--- src/chap02_linear_regression/exercise-linear_regression.py
+++ src/chap02_linear_regression/exercise-linear_regression.py
@@ -220,7 +220,7 @@ def main(x_train, y_train, use_gradient_descent = False, basis_func = None):
 
     def f(x):
         phi0 = np.expand_dims(np.ones_like(x), axis = 1)# 创建全1的列向量作为基函数的第一项（对应截距项）
-        phi1 = basis_func(x)
+        phi1 = basis_func(x) # 调用外部基函数（如多项式、高斯核等）处理输入特征
         phi = np.concatenate([phi0, phi1], axis = 1)
         if use_gradient_descent and w_gd is not None:
             return np.dot(phi, w_gd)
