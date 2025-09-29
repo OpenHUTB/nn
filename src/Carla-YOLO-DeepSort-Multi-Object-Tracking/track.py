694388bbe713b32f25f79f91ae1e2f4f9750dd84
abc123335 <1607504213@qq.com>
2025-09-29 11:19:03 +0800

    添加多对象跟踪项目功能初步实现的文件夹

---
+++ src/Carla-YOLO-DeepSort-Multi-Object-Tracking/track.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from yolo_deepsort import main
+track = main()
+track()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
