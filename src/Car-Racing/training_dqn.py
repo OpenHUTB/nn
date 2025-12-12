8a9552a6b96aa1d894dedb3c479400bb4d16baeb
god <2501229495@qq.com>
2025-12-12 13:50:57 +0800

    为 DQN 训练添加实时奖励曲线 Figure 展示

--- src/Car-Racing/training_dqn.py
+++ src/Car-Racing/training_dqn.py
@@ -1,15 +1,29 @@
 import os
 import sys
+import matplotlib
 import torch
 import datetime
 import csv
+
 import gymnasium as gym
 import gymnasium.wrappers as gym_wrap
+import matplotlib.pyplot as plt
 import numpy as np
+
 # Adjust sys.path to include the parent directory where .dqn_model resides
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import DQN_model as DQN
+
 from gymnasium.spaces import Box
+from tensordict import TensorDict
+from torch import nn
+from torchrl.data import TensorDictReplayBuffer, LazyMemmapStorage
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
 
 # Environment setup
 env = gym.make("CarRacing-v3", continuous=False)  # Explicitly set continuous=False
@@ -22,6 +36,7 @@ state, info = env.reset()
 action_n = env.action_space.n
 print(f"Action space size: {action_n}")  # Debug print
 print(f"Action space details: {env.action_space}")  # Additional debug
+
 # Agent initialization
 driver = DQN.Agent(
     state_space_shape=state.shape,
@@ -46,7 +61,7 @@ when2save = 100000  # in timesteps
 when2report = 5000  # in timesteps
 when2eval = 50000  # in timesteps
 when2log = 10  # in episodes
-report_type = 'text'  # 'text', 'plot', None
+report_type = 'plot'  # 'text', 'plot', None
 
 # Training loop
 while episode < play_n_episodes:
@@ -110,6 +125,8 @@ while episode < play_n_episodes:
     episode_date_list.append(now_time.date().strftime('%Y-%m-%d'))
     episode_time_list.append(now_time.time().strftime('%H:%M:%S'))
 
+    if report_type == 'plot':
+        DQN.plot_reward(episode, episode_reward_list, timestep_n)
 
     if episode % when2log == 0:
         driver.write_log(
@@ -149,6 +166,7 @@ def evaluate_agent(agent, num_episodes=2, render=False):
 
 avg_score = evaluate_agent(driver, num_episodes=2)
 print(f"\nAverage DQN evaluation score: {avg_score:.1f}")
+plt.show()
 
 if report_type == 'text':
     rewards_tensor = torch.tensor(episode_reward_list, dtype=torch.float)
@@ -179,4 +197,4 @@ driver.write_log(
     log_filename='DQN_log_test.csv'
 )
 env.close()
-
+plt.ioff()
