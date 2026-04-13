f5ac66e73e9bff64f47ff73c625660c83a749c19
yao-le-54 <2899659430@qq.com>
2026-04-13 21:29:02 +0800

    AirSim 无人机自主飞行仿真

--- src/drone-flight-sim/main.py
+++ src/drone-flight-sim/main.py
@@ -15,7 +15,7 @@ print("🚁 无人机已起飞！")
 time.sleep(2)
 
 # 自动巡航
-print("🔁 启动自动巡航...")
+print("🔁启动自动巡航...")
 client.moveToPositionAsync(5, 0, -5, 5).join()
 time.sleep(1)
 client.moveToPositionAsync(5, 5, -5, 5).join()
